7ca896d2b3b006c7f7a3e0c48beb2486f03ed8fc
Mason <miaojie134@foxmail.com>
2024-04-13 15:08:20 +0800

    init HomeScreen Authentication routes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { HomeScreen, Authentication } from "../pages";
 
 const App = () => {
-    return (
-        <div>
-            <h1>App</h1>
-        </div>
-    );
-}
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/*" element={<HomeScreen />} />
+        <Route path="/auth" element={<Authentication />} />
+      </Routes>
+    </Suspense>
+  );
+};
 
 export default App;
